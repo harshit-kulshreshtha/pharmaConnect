693ad887113a2424197a345ca692a74a4e540d98
Harshit Kulshrestha <harshitkulshrestha@Harshits-MacBook-Air.local>
2026-09-19 14:29:19 +0530

    fix order api issue

--- models/Medicine.js
+++ models/Medicine.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import "./User";
 
 const MedicineSchema = new mongoose.Schema(
   {
--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import "./User";
+import "./Medicine";
 
 const OrderSchema = new mongoose.Schema(
   {
